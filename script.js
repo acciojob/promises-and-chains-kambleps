f493a53f04aa5125dd62e57882b04f4314ced1d8
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
 //your JS code here. If required.
-const form = document.querySelector("form");
-const nameInput = document.querySelector("#name");
-const ageInput = document.querySelector("#age");
-const submitBtn = document.querySelector("#btn");
+var ageElement= documents.querySelector("#age");
+var nameElement= documents.querySelector("#name");
+var btnElement= documents.querySelector("#btn");
 
-submitBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  if (!nameInput.value || !ageInput.value) {
-    alert("Inputs cannot be empty.");
-    return;
-  }
-
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const age = parseInt(ageInput.value);
-      if (age >= 18) {
-        resolve({ age, name: nameInput.value });
+var promise = new Promise(resolve,reject)=>{
+	setTimeout(()=>{
+		var age=ageElement.value;
+		var name=nameElement.value;
+		if(!(age && name)){
+			alert("please enter a valid details");
+		}
+		return;
+		 const promise = new Promise((resolve, reject) => {
+      if (ageInput.value >= 18) {
+        setTimeout(() => {
+          resolve(`Welcome, ${nameInput.value}. You can vote.`);
+        }, 4000);
       } else {
-        reject({ age, name: nameInput.value });
+        reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
       }
-    }, 4000);
+    });
+
+    promise
+      .then((message) => alert(message))
+      .catch((error) => alert(error));
   });
 
-  promise
-    .then((result) => {
-      const { name } = result;
-      alert(`Welcome, ${name}. You can vote.`);
-      return result.age;
-    })
-    .then((age) => {
-      return { canVote: true, age };
-    })
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      const { name } = error;
-      alert(`Oh sorry ${name}. You aren't old enough.`);
-    });
-});
+}
